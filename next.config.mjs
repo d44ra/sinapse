4bf7288afb7e5780546235557c3e31071afee884
dara <darammbasali@gmail.com>
2026-04-13 20:15:43 -0300

    fix
    git status
    qa!

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   reactStrictMode: true,
 };
 
 export default nextConfig;
-=======
-  /* config options here */
-};
-
-export default nextConfig;
->>>>>>> 3df572335a03f895b230dd8a4fd8fc9a842b6c3c
